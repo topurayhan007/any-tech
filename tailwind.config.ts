9e9798d472a76c1a744b1a94746e178436bf1709
topurayhan007 <topurayhan007@gmail.com>
2025-01-31 22:45:26 +0600

    Update: Added fonts, colors, folder structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,17 @@
 import type { Config } from "tailwindcss";
 
+const customColors = {
+  "blue-main": "#1f80f0",
+  "blue-secondary": "#002045",
+  "blue-tertiary": "#00152D",
+  "blue-medium-light": "#B9D9FF",
+  "blue-light": "#F8FCFF",
+  "orange-main": "#FE8B53",
+  "teal-main": "#00e9ea",
+  "black-main": "#0b305b",
+  "black-secondary": "#164377",
+};
+
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +20,10 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors: customColors,
+      fontFamily: {
+        montserrat: ["var(--font-montserrat)", "Montserrat", "sans-serif"],
+        inter: ["var(--font-inter)", "Inter", "sans-serif"],
       },
     },
   },
