684443b761fd601573f0f970db7292c187cb6ab8
topurayhan007 <topurayhan007@gmail.com>
2025-02-03 07:21:26 +0600

    Update: Hero animation with mouse move done

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,8 +1,28 @@
-import React from "react";
-import Button from "./Button";
+"use client";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import Button from "./Button";
 
 const HeroSection = () => {
+  const [motionValues, setMotionValues] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: MouseEvent) => {
+    const { clientX, clientY } = event;
+    const { innerWidth, innerHeight } = window;
+
+    // Calculate movement direction
+    const x = (clientX - innerWidth / 2) * 0.02; // Adjust sensitivity
+    const y = (clientY - innerHeight / 2) * 0.02;
+
+    setMotionValues({ x, y });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <section className="bg-hero-bg lg:hero-bg-clip relative w-full h-[70vh] md:h-[70vh] lg:h-[94vh] overflow-hidden">
       <div className="container h-full relative z-20 flex items-center justify-center">
@@ -11,7 +31,7 @@ const HeroSection = () => {
             <h1 className="">Embrace the future of finance</h1>
             <p className="text-lg font-semibold text-white my-8 font-montserrat">
               Reimagine financial services with AnyTech’s open platform,
-              distributed banking solution that powers transformation
+              distributed banking solution that powers transformation.
             </p>
             <div className="w-full lg:w-auto">
               <Button href="#" variant="primary">
@@ -35,9 +55,15 @@ const HeroSection = () => {
         <div className="bg-hero-gradient-overlay absolute top-0 left-0 h-full w-full"></div>
       </div>
 
-      <div className="hidden lg:block absolute inset-0 h-full w-full z-0">
+      {/* Animated SVG */}
+      <motion.div
+        className="hidden lg:block absolute inset-0 h-full w-full z-0"
+        style={{
+          transform: `translateX(${motionValues.x}px) translateY(${motionValues.y}px)`,
+        }}
+        transition={{ type: "spring", stiffness: 80, damping: 10 }}
+      >
         <Image
-          // src="/assets/backgrounds/WaveLinesDesktop1.svg"
           src="/assets/backgrounds/heroDesktopWaveLines.svg"
           alt="Angled lines 1"
           width={0}
@@ -45,7 +71,7 @@ const HeroSection = () => {
           sizes="100vw"
           className="object-cover h-full w-full"
         />
-      </div>
+      </motion.div>
 
       <div className="block lg:hidden absolute top-0 left-0 h-full w-full">
         <Image
